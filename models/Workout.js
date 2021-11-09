bb27c671f99e4cc57f43dec1514d5fd91b008231
Jing <jingwang6028@gmail.com>
2021-11-08 21:04:26 -0500

    update model

--- models/Workout.js
+++ models/Workout.js
@@ -2,49 +2,61 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // workout schema
-const WorkoutSchema = new Schema({
-  day: {
-    type: Date,
-    default: () => new Date(),
-  },
-  exercises: [
-    {
-      type: {
-        type: String,
-        trim: true,
-        required: "Please enter exercise type",
-      },
-      name: {
-        type: String,
-        trim: true,
-        required: "Please enter exercise name",
-      },
-      duration: {
-        type: Number,
-        required: "Please enter exercise duration in minutes",
-      },
-      weight: {
-        type: Number,
-        default: 0,
-      },
-      reps: {
-        type: Number,
-        default: 0,
-      },
-      sets: {
-        type: Number,
-        default: 0,
-      },
-      distance: {
-        type: Number,
-        default: 0,
-      },
+const WorkoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: () => new Date(),
     },
-  ],
-  totalDuration: {
-    type: Number,
-    default: 0,
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Please enter exercise type",
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Please enter exercise name",
+        },
+        duration: {
+          type: Number,
+          required: "Please enter exercise duration in minutes",
+        },
+        weight: {
+          type: Number,
+          default: 0,
+        },
+        reps: {
+          type: Number,
+          default: 0,
+        },
+        sets: {
+          type: Number,
+          default: 0,
+        },
+        distance: {
+          type: Number,
+          default: 0,
+        },
+      },
+    ],
   },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// create a virtual property to compute duration
+WorkoutSchema.virtual("totalDuration").get(function () {
+  let totalDuration = 0;
+  this.exercise.forEach((exe) => {
+    totalDuration += exe.duration;
+  });
+  return totalDuration;
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
